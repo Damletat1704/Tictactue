81719999a863edcbe7ca7b83e2cab84d01c9fcfc
Damletat <damletat1704@gmail.com>
2023-07-12 11:21:23 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import { useState } from "react";
+import Board from "./Components/Board";
+import "./App.css"
+export default function Game() {
+  // mảng chứa các bước di chuyển đã thực hiện trong trò chơi
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    // chỉ mục xác định các bước di chuyển hiện tại trong mảng history
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+    function handlePlay(nextSquares) {
+      const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+      setHistory(nextHistory);
+      setCurrentMove(nextHistory.length - 1);
+    }
+  
+    function jumpTo(nextMove) {
+      setCurrentMove(nextMove);
+    }
+    // move là chỉ mục của bước di chuyển mà bạn muốn quay lại.
+    const moves = history.map((squares, move) => {
+      let description;
+      if (move > 0) {
+        description = 'Go to move #' + move;
+      } else {
+        description = 'Go to game start';
+      }
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        </li>
+      );
+    });
+  
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
